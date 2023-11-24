0a06094b745d21f0a7ff76e7d21e21a88e9878a7
jhoelnj <128918915+jhoelnj@users.noreply.github.com>
2023-11-24 16:34:46 -0400

    se crea el modulo conversiones

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hola mundo")
+#print("hola mundo")
+
+#  \n hace un salto de linea
+# \t deja una tabulacion
+#print("1\t1\t1\n0\t0\t0\n1\t1\t1")
+
+
+cadena = "pamela"
+my_number = 10
+numero2 = 5.9
+lista = [1, 3]
+tupla = (1, 5)
+dict = {"clave": 3}
+bol = True
+
+print(type(my_number), type(numero2), type(cadena), type(lista), type(tupla), type(dict), type(bol))
