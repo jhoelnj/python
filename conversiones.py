3e36ff884fa29e56dcff61b4d5ff923be0acd23a
jhoelnj <128918915+jhoelnj@users.noreply.github.com>
2023-11-24 17:16:06 -0400

    se crea el modulo cadenas

---
+++ conversiones.py
@@ -0,0 +1,12 @@
+'''num = 5
+letra = str(num)
+print(type(letra))
+
+num2 = int(letra) + 1
+print(num2)
+
+#redondear numeros
+numero = 987.345667
+print(round(numero))
+print(round(numero, 2))'''
+
